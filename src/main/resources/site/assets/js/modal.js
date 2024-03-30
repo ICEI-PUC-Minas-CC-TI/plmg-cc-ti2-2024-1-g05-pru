769a976b607b5078137230ad774aa9ce02ff4b3c
André Luís <andreluis.silvap@gmail.com>
2024-03-30 00:10:23 -0300

    add modal css and js

---
+++ src/main/resources/site/assets/js/modal.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById('modal');
+
+document.querySelector('#add-consulta').addEventListener('click', () => {
+  modal.style.display = 'block';
+}, false);
+
+document.querySelector('#close-modal').addEventListener('click', () => {
+  modal.style.display = 'none';
+}, false);
+
+window.addEventListener('click', (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}, false);
