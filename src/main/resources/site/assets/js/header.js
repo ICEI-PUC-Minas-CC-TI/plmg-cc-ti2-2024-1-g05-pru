95b89dc97324a4b3c58c156d62c5bbfe8cd95272
André Luís <andreluis.silvap@gmail.com>
2024-04-03 23:00:06 -0300

    HOTFIX logout button

--- src/main/resources/site/assets/js/header.js
+++ src/main/resources/site/assets/js/header.js
@@ -3,8 +3,10 @@ window.addEventListener('load', loadHeader, false);
 function loadHeader() {
   const login = document.querySelector('header nav .menu li.login');
   const profile = document.querySelector('header nav .menu li.profile');
+  const logout = document.querySelector('header nav .menu li.logout');
   const isUserSessionExists = userSessionExists();
 
   login.style.display = isUserSessionExists ? 'none' : 'block';
   profile.style.display = isUserSessionExists ? 'block' : 'none';
+  logout.style.display = isUserSessionExists ? 'flex' : 'none';
 }
